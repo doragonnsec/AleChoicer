73ca318d743415b2d97af4e33a663a0247fed101
ericrunnerr <eric.marquesgomesmarques@gmail.com>
2022-10-20 10:58:43 -0300

    Primeiro commit do exec

---
+++ choicer.py
@@ -0,0 +1,8 @@
+from random import choice
+
+# Lets see what you will watch today
+animes = ['Bleach', 'Jujutsu Kaisen', 'Kimetsu no Yaiba',
+          'Blue Lock', 'One Piece', 'Slice of life', 'Horror']
+
+# And the winner is!
+print(choice(animes))
